fc07902dff09b02c3a8193686fcbc232ed2ab471
boz <david.bosley@hpe.com>
2023-02-01 07:05:38 -0800

    Docker support

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,39 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
 
 export function divide(a: number, b: number): number {
-  // watch for divide by zero
-  if (b === 0) {
-    throw new Error("You can't divide by zero.");
-  }
+    // watch for divide by zero
+    if (b === 0) {
+        throw new Error("You can't divide by zero.")
+    }
 
-  return a / b;
+    return a / b
 }
 
 export function add(a: number, b: number): number {
-  return a + b;
+    return a + b
 }

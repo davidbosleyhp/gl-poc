5b509bf994118b9f6446deb7351fbc6a02c34eeb
Saroj Ekka <saroj.ekka@hpe.com>
2023-02-11 00:59:40 -0800

    some more folder added

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'styles/index.css'
+import 'styles/css/index.css'
 import App from 'layouts/app'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
